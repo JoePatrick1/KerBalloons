2466b1e9828e872af9201bfc5a382796e99a95b9
JoePatrick1 <skittlesandfantamakemehyper@gmail.com>
2016-08-16 12:48:21 +0100

    Test
    Test

---
+++ KerBalloons/KerBalloons/BalloonProperties.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace KerBalloons
+{
+    public class BalloonProperties
+    { 
+        public static float getLift(ModuleKerBalloon thisBalloon)
+        {
+            
+            float atmoPressure = (float)FlightGlobals.getStaticPressure(thisBalloon.part.transform.position);
+            float coefficient = (thisBalloon.minLift - thisBalloon.maxLift) / Mathf.Pow(thisBalloon.maxAtmoPressure, 2);
+            float x = Mathf.Pow(atmoPressure - thisBalloon.maxAtmoPressure - thisBalloon.minAtmoPressure, 2);
+            float yInt = thisBalloon.maxLift;
+
+            float lift = coefficient * x + yInt;
+            float max = lift;
+
+            float liftLimit = (thisBalloon.vessel.GetTotalMass() * (float)FlightGlobals.getGeeForceAtPosition(thisBalloon.transform.position).magnitude) / lift;
+            lift *= liftLimit * thisBalloon.targetTWR;
+            
+
+            if (thisBalloon.speedLimiter)
+            {
+                if (thisBalloon.vessel.verticalSpeed < thisBalloon.maxSpeed * (1 - thisBalloon.maxSpeedTolerence))
+                {
+                    thisBalloon.speedAdjust += thisBalloon.speedAdjustStep * (thisBalloon.maxSpeed - (float)thisBalloon.vessel.verticalSpeed);
+                }
+                else if (thisBalloon.vessel.verticalSpeed > thisBalloon.maxSpeed * (1 + thisBalloon.maxSpeedTolerence))
+                {
+                    thisBalloon.speedAdjust -= thisBalloon.speedAdjustStep * ((float)thisBalloon.vessel.verticalSpeed - thisBalloon.maxSpeed);
+                }
+                thisBalloon.speedAdjust = Mathf.Clamp(thisBalloon.speedAdjust, thisBalloon.speedAdjustMin, thisBalloon.speedAdjustMax);
+                lift *= thisBalloon.speedAdjust;
+            }
+            if(thisBalloon.isInflated || thisBalloon.isInflating) lift /= getInflatedBalloons(thisBalloon.vessel).Count;
+            lift = Mathf.Clamp(lift, 0, max);
+            return lift;
+        }
+
+
+        public static float getScale(ModuleKerBalloon thisBalloon)
+        {
+            float atmoPressure = (float)FlightGlobals.getStaticPressure(thisBalloon.part.transform.position);
+            float coefficient = (thisBalloon.maxScale - thisBalloon.minScale) / Mathf.Pow(thisBalloon.maxAtmoPressure, 2);
+            float x = Mathf.Pow(atmoPressure - thisBalloon.maxAtmoPressure - thisBalloon.minAtmoPressure, 2);
+            float yInt = thisBalloon.minScale;
+
+            float scale = coefficient * x + yInt;
+
+            return scale;
+        }
+
+        public static List<ModuleKerBalloon> getInflatedBalloons(Vessel vessel)
+        {
+            List<ModuleKerBalloon> balloons = new List<ModuleKerBalloon>();
+            foreach(Part part in vessel.parts)
+            {
+                if (part.GetComponent<ModuleKerBalloon>())
+                {
+                    ModuleKerBalloon balloon = part.GetComponent<ModuleKerBalloon>();
+                    if ((balloon.isInflated || balloon.isInflating) && !balloon.hasBurst)
+                    {
+                        balloons.Add(balloon);
+                    }
+                }
+            }
+            return balloons;
+        }
+    }
+}
---
+++ KerBalloons/KerBalloons/KerBalloonsCatagory.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using KSP.UI;
+using KSP.UI.Screens;
+using Icon = RUI.Icons.Selectable.Icon;
+
+
+namespace KerBalloons
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    class KerBalloonsCatagory : MonoBehaviour
+    {
+        private static readonly List<AvailablePart> availableParts = new List<AvailablePart>();
+
+        private void Awake()
+        {
+
+            availableParts.Clear();
+            foreach (var part in PartLoader.LoadedPartsList)
+            {
+                if (!part.partPrefab) continue;
+                if (part.manufacturer == "KerBalloons")
+                {
+                    availableParts.Add(part);
+                }
+            }
+
+            GameEvents.onGUIEditorToolbarReady.Add(SubCategory);
+          
+        }
+
+        
+        private void SubCategory()
+        { 
+            Debug.Log("Adding KerBalloons Catagory");
+            const string FILTER_CATEGORY = "Filter by Function";
+            const string CUSTOM_CATEGORY_NAME = "KerBalloons";
+
+            
+
+            Texture2D iconTexNormal = GameDatabase.Instance.GetTexture("KerBalloons/Textures/KBIconNormal", false);
+            Texture2D iconTexSelected = GameDatabase.Instance.GetTexture("KerBalloons/Textures/KBIconSelected", false);
+
+            Icon icon = new Icon("KerBalloons", iconTexNormal, iconTexSelected, false);
+
+            PartCategorizer.Category filter = PartCategorizer.Instance.filters.Find(f => f.button.categoryName == FILTER_CATEGORY);
+            PartCategorizer.AddCustomSubcategoryFilter(filter, CUSTOM_CATEGORY_NAME, icon, p => availableParts.Contains(p));
+
+            UIRadioButton button = filter.button.activeButton;
+        }
+    }
+}
+
